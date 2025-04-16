b66fc1e68327213cc689fbec36e4ed524b0ae5b3
NazarSlobodian <nazar.slobodian.pz.2022@lpnu.ua>
2025-04-16 20:20:28 +0300

    Component update

--- meds.client/src/app/admin/activity-logs/activity-logs.component.ts
+++ meds.client/src/app/admin/activity-logs/activity-logs.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivityLogsService } from './services/activity-logs.service';
 
 @Component({
   selector: 'app-activity-logs',
@@ -6,5 +8,51 @@ import { Component } from '@angular/core';
   styleUrl: './activity-logs.component.css'
 })
 export class ActivityLogsComponent {
+  begin: Date = new Date(2025, 4, 1, 0, 0, 0, 0);
+  end: Date = new Date(2025, 5, 1, 0, 0, 0, 0);
 
+  logs: any[] = [];
+
+  pageSize = 20;
+  currentPage = 1;
+  totalPages = 1;
+  totalCount = 0;
+
+  errorMessage: string | null = null;
+
+  columnNames: string[] = [];
+  constructor(private router: Router, private activityLogsService: ActivityLogsService) { }
+
+  ngOnInit() {
+
+  }
+
+  loadLogs() {
+    this.activityLogsService.getActivityLogs(this.begin, this.end, this.currentPage, this.pageSize).subscribe({
+      next: (response) => {
+        this.logs = response.list;
+        this.totalPages = Math.ceil(response.totalCount / this.pageSize);
+        this.totalCount = response.totalCount;
+        this.errorMessage = null;
+        this.columnNames = Object.keys(this.logs[0]);
+      },
+      error: (error) => {
+        this.logs = [];
+        this.totalPages = 1;
+        this.totalCount = 0;
+        this.currentPage = 1;
+        this.errorMessage = error.error.message;
+      }
+    });
+  }
+  onPageChange(page: number): void {
+    this.currentPage = page;
+    this.loadLogs();
+    if (this.currentPage > this.totalPages)
+      this.currentPage = this.totalPages;
+  }
+
+  goBack(): void {
+    this.router.navigate(["/admin/options/"]);
+  }
 }
---
+++ meds.client/src/app/admin/activity-logs/services/activity-logs.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActivityLogsService {
+  private apiUrl = "https://localhost:7217/Statistics/activityLogs";
+  constructor(private http: HttpClient) { }
+  getActivityLogs(begin: Date, end: Date, page: number, pageSize: number): Observable<any> {
+    return this.http.post(`${this.apiUrl}`, { begin: begin, end: end }, { withCredentials: true });
+  }
+}
